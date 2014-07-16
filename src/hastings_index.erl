afed4806b4a8b62cbdef53aaf0221da51f2a75c3
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-07-16 04:47:57 -0500

    Set the sig properly and return the index

--- src/hastings_index.erl
+++ src/hastings_index.erl
@@ -270,7 +270,8 @@ set_index_sig(Idx) ->
         Idx#h_idx.dimensions,
         Idx#h_idx.srid
     },
-    ?l2b(couch_util:to_hex(couch_util:md5(term_to_binary(SigTerm)))).
+    Sig = ?l2b(couch_util:to_hex(couch_util:md5(term_to_binary(SigTerm)))),
+    Idx#h_idx{sig = Sig}.
 
 
 get_index_def(IdxProps) ->
