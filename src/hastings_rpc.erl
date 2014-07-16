b3cee833da466d4e22c0884f420ab685ef277cef
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-07-15 23:33:07 -0500

    Refactor hastings_rpc
    General cleanup.

--- src/hastings_rpc.erl
+++ src/hastings_rpc.erl
@@ -7,77 +7,67 @@
 -include("hastings.hrl").
 
 
--import(couch_query_servers, [
-    get_os_process/1,
-    ret_os_process/1,
-    proc_prompt/2
-]).
-
-
 -export([
     search/4,
     info/3
 ]).
 
 
-search(DbName, DDoc, IndexName, QueryArgs) ->
+search(DbName, DDoc, IndexName, HQArgs) ->
     erlang:put(io_priority, {interactive, DbName}),
-    {ok, Db} = get_or_create_db(DbName, []),
-    #index_query_args{
-        stale = Stale
-    } = QueryArgs,
-
-    {_LastSeq, MinSeq} = calculate_seqs(Db, Stale),
-    case hastings_index:design_doc_to_index(DDoc, IndexName) of
-        {ok, Index} ->
-            case hastings_index_manager:get_index(DbName, Index) of
-                {ok, Pid} ->
-                    case hastings_index:await(Pid, MinSeq) of
-                        ok ->
-                            Result = hastings_index:search(Pid, QueryArgs),
-                            rexi:reply(Result);
-                        Error ->
-                            rexi:reply(Error)
-                    end;
-                Error ->
-                    rexi:reply(Error)
-            end;
-        Error ->
-            rexi:reply(Error)
-    end.
+    AwaitSeq = get_await_seq(DbName, HQArgs),
+    Pid = get_index_pid(DbName, DDoc, IndexName),
+    case hastings_index:await(Pid, AwaitSeq) of
+        ok -> ok;
+        Error -> reply(Error)
+    end,
+    reply(hastings_index:search(Pid, QueryArgs)).
 
 
 info(DbName, DDoc, IndexName) ->
     erlang:put(io_priority, {interactive, DbName}),
-    case hastings_index:design_doc_to_index(DDoc, IndexName) of
-        {ok, Index} ->
-            case hastings_index_manager:get_index(DbName, Index) of
-                {ok, Pid} ->
-                    Result = hastings_index:info(Pid),
-                    rexi:reply(Result);
-                Error ->
-                    rexi:reply(Error)
-            end;
-        Error ->
-            rexi:reply(Error)
+    Pid = get_index_pid(DbName, DDoc, IndexName),
+    reply(hastings_index:info(Pid)).
+
+
+get_index_pid(DbName, DDoc, IndexName) ->
+    Index = case hastings_index:design_doc_to_index(DDoc, IndexName) of
+        {ok, Index0} -> Index0;
+        Error -> reply(Error)
+    end,
+    case hastings_index_manager:get_index(DbName, Index) of
+        {ok, Pid} -> Pid;
+        Error -> reply(Error)
+    end.
+
+
+get_await_seq(DbName, HQArgs) ->
+    case HQArgs#hq_args.stale of
+        true -> 0;
+        update_after -> 0;
+        false -> get_update_seq(DbName)
+    end.
+
+
+get_update_seq(DbName) ->
+    {ok, Db} = get_or_create_db(Db, []),
+    try
+        LastSeq = couch_db:get_update_seq(Db)
+    after
+        couch_db:close(Db)
     end.
 
 
 get_or_create_db(DbName, Options) ->
     case couch_db:open_int(DbName, Options) of
-    {not_found, no_db_file} ->
-        twig:log(warn, "~p creating ~s", [?MODULE, DbName]),
-        couch_server:create(DbName, Options);
-    Else ->
-        Else
+        {not_found, no_db_file} ->
+            twig:log(warn, "~s creating ~s", [?MODULE, DbName]),
+            couch_server:create(DbName, Options);
+        Else ->
+            Else
     end.
 
 
-calculate_seqs(Db, Stale) ->
-    LastSeq = couch_db:get_update_seq(Db),
-    if
-        Stale == ok orelse Stale == update_after ->
-            {LastSeq, 0};
-        true ->
-            {LastSeq, LastSeq}
-    end.
+reply(Msg) ->
+    rexi:reply(Msg),
+    exit(normal).
