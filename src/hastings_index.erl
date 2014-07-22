810d40e1ebc4522ddd43060696cee84babc328dd
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-07-21 19:40:27 -0500

    Add an index soft limit and upgrade command
    This works on by having each index pid report back to the manager
    periodically when it doesn't have any clients. If its generation is out
    of date or if the maximum number of pids is open then it will close the
    index. In theory this will prefer to close idle indices over used
    indices as active clients will prevent an index from checking if it
    needs to close.

--- src/hastings_index.erl
+++ src/hastings_index.erl
@@ -9,9 +9,13 @@
 
 
 -export([
-    start_link/2,
+    start_link/4,
+    stop/1,
+
     destroy/1,
     design_doc_to_index/2,
+
+    add_monitor/2,
 
     await/2,
     search/2,
@@ -33,15 +37,36 @@
 
 
 -record(st, {
+    manager,
     index,
-    dbref,
+    dbpid,
     updater_pid,
-    waiting_list = []
+    waiting_list = [],
+    generation
 }).
 
 
-start_link(DbName, Index) ->
-    proc_lib:start_link(?MODULE, init, [{DbName, Index}]).
+start_link(Manager, DbName, Index, Generation) ->
+    proc_lib:start_link(?MODULE, init, [{Manager, DbName, Index, Generation}]).
+
+
+stop(Pid) ->
+    Ref = erlang:monitor(process, Pid),
+    try
+        case gen_server:call(Pid, stop) of
+            false ->
+                false;
+            ok ->
+                receive
+                    {'DOWN', Ref, _, _, _} ->
+                        ok
+                after 50000 ->
+                    throw({timeout, closing_index})
+                end
+        end
+    after
+        erlang:demonitor(Ref, [flush])
+    end.
 
 
 destroy(IndexDir) ->
@@ -54,6 +79,13 @@ design_doc_to_index(#doc{id=Id, body={Fields}}, IndexName) ->
     catch throw:Error ->
         Error
     end.
+
+
+add_monitor(Pid, {Client, _}) ->
+    add_monitor(Pid, Client);
+
+add_monitor(Pid, Client) when is_pid(Client) ->
+    gen_server:cast(Pid, {add_monitor, Client}).
 
 
 await(Pid, MinSeq) ->
@@ -80,20 +112,23 @@ remove(Pid, Id) ->
     gen_server:call(Pid, {remove, Id}, infinity).
 
 
-init({DbName, Index}) ->
+init({Manager, DbName, Index, Generation}) ->
     process_flag(trap_exit, true),
+    erlang:send_after(get_timeout(), self(), run_checks),
     case open_index(DbName, Index) of
         {ok, NewIndex} ->
             {ok, Db} = couch_db:open_int(DbName, []),
-            DbRef = try
+            try
                 couch_db:monitor(Db)
             after
                 couch_db:close(Db)
             end,
             proc_lib:init_ack({ok, self()}),
             St = #st{
+                manager = Manager,
                 index = NewIndex,
-                dbref = DbRef
+                dbpid = Db#db.main_pid,
+                generation = Generation
             },
             gen_server:enter_loop(?MODULE, [], St);
         Error ->
@@ -106,6 +141,19 @@ terminate(_Reason, St) ->
     catch exit(St#st.updater_pid, kill),
     ok = easton_index:close(Index#h_idx.pid).
 
+
+handle_call(stop, _From, St) ->
+    % We know that the index manager will send us
+    % no more clients after this message but we may
+    % have a message in our mailbox. This means
+    % we need to check if there's a client that just
+    % came in before we check if we need to stop.
+    case add_last_monitors() of
+        true ->
+            {reply, false, St};
+        false ->
+            {stop, normal, ok, St}
+    end;
 
 handle_call({await, RequestSeq}, From, St) ->
     #st{
@@ -168,9 +216,24 @@ handle_call({remove, Id}, _From, St) ->
     {reply, Resp, St}.
 
 
+handle_cast({add_monitor, Pid}, St) ->
+    erlang:monitor(process, Pid),
+    {noreply, St};
+
 handle_cast(_Msg, St) ->
     {noreply, St}.
 
+
+handle_info(run_checks, St) ->
+    erlang:send_after(get_timeout(), self(), run_checks),
+    case has_clients(St) of
+        true ->
+            ok;
+        false ->
+            Msg = {gen_check, self(), St#st.generation},
+            gen_server:cast(St#st.manager, Msg)
+    end,
+    {noreply, St};
 
 handle_info({'EXIT', Pid, {updated, NewSeq}}, #st{updater_pid = Pid} = St) ->
     Index0 = St#st.index,
@@ -201,11 +264,13 @@ handle_info({'EXIT', Pid, Reason}, #st{index=#h_idx{pid={Pid}}} = St) ->
     ?LOG_INFO(Fmt, [index_name(St#st.index), Reason]),
     [gen_server:reply(P, {error, Reason}) || {P, _} <- St#st.waiting_list],
     {stop, normal, St};
-handle_info({'DOWN', DbRef, _, Pid, Reason}, #st{dbref=DbRef} = St) ->
+handle_info({'DOWN', _, _, DbPid, Reason}, #st{dbpid=DbPid} = St) ->
     Fmt = "~s ~s closing: Db pid ~p closing w/ reason ~w",
-    ?LOG_INFO(Fmt, [?MODULE, index_name(St#st.index), Pid, Reason]),
+    ?LOG_INFO(Fmt, [?MODULE, index_name(St#st.index), DbPid, Reason]),
     [gen_server:reply(P, {error, Reason}) || {P, _} <- St#st.waiting_list],
-    {stop, normal, St}.
+    {stop, normal, St};
+handle_info({'DOWN', _, _, _, _}, St) ->
+    {noreply, St}.
 
 
 code_change(_OldVsn, St, _Extra) ->
@@ -242,6 +307,33 @@ reply_with_index(Index, WaitList) ->
                 [W | Acc]
         end
     end, [], WaitList).
+
+
+has_clients(St) ->
+    DbPid = St#st.dbpid,
+    {monitors, Monitors} = process_info(self(), monitors),
+    case Monitors of
+        [] ->
+            false;
+        [{process, DbPid}] ->
+            false;
+        _Else ->
+            true
+    end.
+
+
+add_last_monitors() ->
+    add_last_monitors(false).
+
+
+add_last_monitors(Resp) ->
+    receive
+        {'$gen_cast', {add_monitor, Client}} ->
+            erlang:monitor(process, Client),
+            add_last_monitors(true)
+    after 0 ->
+        Resp
+    end.
 
 
 design_doc_to_index_int(Id, Fields, IndexName) ->
@@ -350,3 +442,12 @@ index_name(#h_idx{dbname=DbName, ddoc_id=DDocId, name=IndexName}) ->
 index_directory(DbName, Sig) ->
     GeoDir = config:get("hastings", "geo_index_dir", "/srv/geo_index"),
     filename:join([GeoDir, DbName, Sig]).
+
+
+get_timeout() ->
+    TO = config:get("hastings", "index_timeout", "60000"),
+    try
+        list_to_integer(TO)
+    catch _:_ ->
+        60000
+    end.
--- src/hastings_index_manager.erl
+++ src/hastings_index_manager.erl
@@ -14,7 +14,8 @@
 
 -export([
     start_link/0,
-    get_index/2
+    get_index/2,
+    upgrade/0
 ]).
 
 -export([
@@ -26,6 +27,15 @@
     code_change/3
 ]).
 
+-export([
+    new_index/4
+]).
+
+
+-record(st, {
+    generation = 0
+}).
+
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -35,11 +45,15 @@ get_index(DbName, Index) ->
     gen_server:call(?MODULE, {get_index, DbName, Index}, infinity).
 
 
+upgrade() ->
+    gen_server:call(?MODULE, upgrade, infinity).
+
+
 init([]) ->
     ets:new(?BY_SIG, [set, public, named_table]),
     ets:new(?BY_PID, [set, public, named_table]),
     process_flag(trap_exit, true),
-    {ok, nil}.
+    {ok, #st{}}.
 
 
 terminate(_Reason, _State) ->
@@ -47,17 +61,26 @@ terminate(_Reason, _State) ->
 
 
 handle_call({get_index, DbName, #h_idx{sig=Sig}=Index}, From, State) ->
+    Gen = State#st.generation,
     case ets:lookup(?BY_SIG, {DbName, Sig}) of
     [] ->
-        spawn_link(fun() -> new_index(DbName, Index) end),
+        spawn_link(?MODULE, new_index, [self(), DbName, Index, Gen]),
         ets:insert(?BY_SIG, {{DbName,Sig}, [From]}),
         {noreply, State};
     [{_, WaitList}] when is_list(WaitList) ->
         ets:insert(?BY_SIG, {{DbName, Sig}, [From | WaitList]}),
         {noreply, State};
     [{_, ExistingPid}] ->
+        hastings_index:add_monitor(ExistingPid, From),
         {reply, {ok, ExistingPid}, State}
     end;
+
+handle_call(upgrade, _From, State) ->
+    Gen = State#st.generation,
+    NewState = State#st{
+        generation = Gen + 1
+    },
+    {reply, Gen + 1, NewState};
 
 handle_call({open_ok, DbName, Sig, NewPid}, _From, State) ->
     link(NewPid),
@@ -72,9 +95,35 @@ handle_call({open_error, DbName, Sig, Error}, _From, State) ->
     ets:delete(?BY_SIG, {DbName, Sig}),
     {reply, ok, State}.
 
+
+handle_cast({gen_check, FromPid, Gen}, State) ->
+    case ets:lookup(?BY_PID, FromPid) of
+        [] ->
+            % An old index that we have since forgotten about.
+            % Its probably just indexing so ignore it and it'll
+            % go away on its own.
+            ok;
+        [{_, {DbName, Sig}}] ->
+            CurrGen = Gen == State#st.generation,
+            MaxOpen = ets:info(?BY_PID, size) > get_max_open(),
+            case CurrGen andalso not MaxOpen of
+                true ->
+                    ok;
+                false ->
+                    case hastings_index:stop(FromPid) of
+                        ok ->
+                            delete_from_ets(FromPid, DbName, Sig);
+                        false ->
+                            ok
+                    end
+            end
+    end,
+    {noreply, State};
+
 handle_cast({cleanup, DbName}, State) ->
     hastings_vacuum:cleanup(DbName),
     {noreply, State}.
+
 
 handle_info({'EXIT', FromPid, Reason}, State) ->
     case ets:lookup(?BY_PID, FromPid) of
@@ -97,8 +146,8 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 
-new_index(DbName, #h_idx{sig=Sig}=Index) ->
-    case hastings_index:start_link(DbName, Index) of
+new_index(Manager, DbName, #h_idx{sig=Sig}=Index, Generation) ->
+    case hastings_index:start_link(Manager, DbName, Index, Generation) of
     {ok, NewPid} ->
         Msg = {open_ok, DbName, Sig, NewPid},
         ok = gen_server:call(?MODULE, Msg, infinity),
@@ -117,3 +166,12 @@ add_to_ets(Pid, DbName, Sig) ->
 delete_from_ets(Pid, DbName, Sig) ->
     true = ets:delete(?BY_PID, Pid),
     true = ets:delete(?BY_SIG, {DbName, Sig}).
+
+
+get_max_open() ->
+    MO = config:get("hastings", "open_index_soft_limit", "100"),
+    try
+        list_to_integer(MO)
+    catch _:_ ->
+        100
+    end.
