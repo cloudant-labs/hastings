9da86972523c210800f5e92547e7124af0be5c59
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-07-24 18:53:25 -0500

    Catch and log errors when updating an index
    If we don't catch errors here we fail to make progress indexing. To be a
    bit on the safe side we attempt to remove any entry that failed to
    update so that we don't end up with index entries from a previous
    version that was updated successfully.

--- src/hastings_index_updater.erl
+++ src/hastings_index_updater.erl
@@ -22,7 +22,8 @@
     proc,
     changes_done = 0,
     total_changes,
-    prev_cp = os:timestamp()
+    prev_cp = os:timestamp(),
+    name
 }).
 
 
@@ -64,7 +65,8 @@ update(IndexPid, Index) ->
                 idx_pid = IndexPid,
                 db = Db,
                 proc = Proc,
-                total_changes = TotalChanges
+                total_changes = TotalChanges,
+                name = index_name(DbName, DDocId, IndexName)
             },
             {ok, _, _} = couch_db:enum_docs_since(Db, UpSeq, EnumFun, Acc0, []),
             hastings_index:set_update_seq(IndexPid, NewSeq)
@@ -94,9 +96,22 @@ load_docs(FDI, _, Acc) ->
             Args = [<<"st_index_doc">>, couch_doc:to_json_obj(Doc, [])],
             [Geoms0] = ?CQS:proc_prompt(Acc#acc.proc, Args),
             Geoms = [G || [G, _Opts] <- Geoms0],
-            case Geoms of
-                [] -> ok = hastings_index:remove(Acc#acc.idx_pid, Id);
-                _  -> ok = hastings_index:update(Acc#acc.idx_pid, Id, Geoms)
+            try
+                case Geoms of
+                    [] -> ok = hastings_index:remove(Acc#acc.idx_pid, Id);
+                    _  -> ok = hastings_index:update(Acc#acc.idx_pid, Id, Geoms)
+                end
+            catch T:R ->
+                % If we failed to update the index then we'll try and
+                % remove the geometry at least. If we get an error here
+                % we're pretty much screwed anyway so hopefully the
+                % log message will give us enough info to be able to fix
+                % the bug.
+                if length(Geoms) == 0 -> ok; true ->
+                    catch hastings_index:remove(Acc#acc.idx_pid, Id)
+                end,
+                Args = [Id, Acc#acc.name, {T, R}],
+                twig:log(warn, "Error updating ~p for ~p :: ~p", Args)
             end
     end,
 
@@ -108,3 +123,7 @@ load_docs(FDI, _, Acc) ->
         false ->
             {ok, Acc#acc{changes_done = ChangesDone + 1}}
     end.
+
+
+index_name(DbName, DDocId, IndexName) ->
+    <<DbName/binary, " ", DDocId/binary, " ", IndexName/binary>>.
