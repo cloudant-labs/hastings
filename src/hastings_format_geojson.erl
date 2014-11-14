a7d5123d633b34aec04859bab3d64d44ce281d69
Norman Barker <norman@cloudant.com>
2014-11-14 12:12:30 -0700

    Update result formatting behavior. BugzId: 39538.

---
+++ src/hastings_format_geojson.erl
@@ -0,0 +1,50 @@
+-module(hastings_format_geojson).
+-behaviour(hastings_format).
+
+-include("hastings.hrl").
+
+-export([hits_to_json/2]).
+
+hits_to_json(Hits, HQArgs) ->
+    Bookmark = hastings_bookmark:update(HQArgs#h_args.bookmark, Hits),
+    BookmarkJson = hastings_bookmark:pack(Bookmark),
+    {[], hits_to_json0(Hits, BookmarkJson)}.
+
+hits_to_json0(Hits, Bookmark) ->
+    Geoms = lists:map(fun(H) ->
+        Geom = case H#h_hit.geom of
+            undefined -> null;
+            Geom0 -> Geom0
+        end,
+        ExtraProps = case H#h_hit.doc of
+            undefined -> [{<<"type">>, <<"Feature">>}, {<<"properties">>, []}];
+            {DocList0} ->
+                Props0 = case lists:keyfind(<<"properties">>, 1, DocList0) of 
+                    {_, Props} -> Props;
+                _ -> []
+                end,
+                Rev0 = case lists:keyfind(<<"_rev">>, 1, DocList0) of 
+                    {_, Rev} -> Rev;
+                    _ -> null
+                end,
+                Type0 = case lists:keyfind(<<"type">>, 1, DocList0) of
+                    {_, Type} -> Type;
+                    _ -> <<"Feature">>
+                end,
+                [
+                    {<<"_rev">>, Rev0},
+                    {<<"type">>, Type0},
+                    {<<"properties">>, Props0}
+                ]
+        end,
+        {[
+            {<<"_id">>, H#h_hit.id},
+            {<<"geometry">>, Geom}
+            | ExtraProps
+        ]}
+    end, Hits),
+    {[
+        {<<"bookmark">>, Bookmark},
+        {<<"type">>, <<"FeatureCollection">>},
+        {<<"features">>, Geoms}
+    ]}.
