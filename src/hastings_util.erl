705e409f291c3c666a7fe39bf5fd31c2631a9d6f
Paul J. Davis <paul.joseph.davis@gmail.com>
2015-02-13 12:12:34 -0600

    Fix include_docs=true for new view engine
    The callback values for the new view engine use a slightly different
    format for messages. This supports both the old and new format.
    
    BugzId: 44364

--- src/hastings_util.erl
+++ src/hastings_util.erl
@@ -16,11 +16,15 @@ get_json_docs(DbName, DocIds) ->
     fabric:all_docs(DbName, fun callback/2, [], Opts).
 
 
+callback({meta, _}, Acc) ->
+    {ok, Acc};
 callback({total_and_offset,_,_}, Acc) ->
     {ok, Acc};
 callback({error, Reason}, _Acc) ->
     {error, Reason};
 callback({row, {Props}}, Acc) ->
+    callback({row, Props}, Acc);
+callback({row, Props}, Acc) when is_list(Props) ->
     {id, Id} = lists:keyfind(id, 1, Props),
     {doc, Doc} = lists:keyfind(doc, 1, Props),
     {ok, [{Id, Doc} | Acc]};
