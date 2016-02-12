9361e1e2e21e3b75f21c9fed6c3f895a709276d3
Mike Wallace <mikewallace@apache.org>
2016-02-12 12:25:04 +0000

    Implement a legacy output format for Local
    This legacy format is very similar to the view format however the
    rows are returned under a "features" key. This matches the format
    used for Local releases 1.0.0.0 to 1.0.0.4.
    
    It is not used by default but can be enabled by setting the
    hastings/default_format config variable to "legacy_local".
    
    This is intended as a temporary stop gap for Cloudant Local
    customers until we can switch them to the mainline format in the
    next proper release of Local.
    
    BugzId: 60910

---
+++ src/hastings_format_legacy_local.erl
@@ -0,0 +1,37 @@
+-module(hastings_format_legacy_local).
+
+-include("hastings.hrl").
+
+-export([hits_to_json/2]).
+
+hits_to_json(Hits, HQArgs) ->
+    Bookmark = hastings_bookmark:update(HQArgs#h_args.bookmark, Hits),
+    BookmarkJson = hastings_bookmark:pack(Bookmark),
+    {[], hits_to_json0(Hits, BookmarkJson)}.
+
+
+hits_to_json0(Hits, Bookmark) ->
+    Docs = lists:map(fun(H) ->
+        Geom = case H#h_hit.geom of
+            undefined -> null;
+            Geom0 -> Geom0
+        end,
+        case H#h_hit.doc of
+            undefined ->
+                {[
+                    {<<"id">>, H#h_hit.id},
+                    {<<"geometry">>, Geom}
+                ]};
+            Doc ->
+                {[
+                    {<<"id">>, H#h_hit.id},
+                    {<<"geometry">>, Geom},
+                    {doc, Doc}
+                ]}
+        end
+    end, Hits),
+    {[
+        {<<"type">>, <<"FeatureCollection">>},
+        {<<"bookmark">>, Bookmark},
+        {<<"features">>, Docs}
+    ]}.
