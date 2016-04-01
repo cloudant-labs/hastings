d4c8dfb67a9f26ba0ac7032d3f30c4a09441730a
jiangphcn <jiangph@cn.ibm.com>
2016-04-01 17:35:53 +0800

    store the doc instead of storing rev in h_hit.doc, and then retrieve the rev
    BugzID: 60055

--- src/hastings_format_view.erl
+++ src/hastings_format_view.erl
@@ -11,7 +11,7 @@ hits_to_json(DbName, Hits, HQArgs) ->
 
 
 hits_to_json0(DbName, Hits, HQArgs, Bookmark) ->
-    Hits0 = maybe_add_revs(DbName, Hits, HQArgs),
+    Hits0 = maybe_add_docs_in_view_format(DbName, Hits, HQArgs),
     Docs = lists:map(fun(H) ->
         Geom = case H#h_hit.geom of
             undefined -> null;
@@ -19,9 +19,10 @@ hits_to_json0(DbName, Hits, HQArgs, Bookmark) ->
         end,
         case HQArgs#h_args.include_docs of
             false ->
+                {_, Rev} = lists:keyfind(<<"_rev">>, 1,H#h_hit.doc),
                 {[
                     {<<"id">>, H#h_hit.id},
-                    {<<"rev">>, H#h_hit.doc},
+                    {<<"rev">>, Rev},
                     {<<"geometry">>, Geom}
                 ]};
             true ->
@@ -38,17 +39,16 @@ hits_to_json0(DbName, Hits, HQArgs, Bookmark) ->
     ]}.
 
 
-maybe_add_revs(DbName, Hits, #h_args{include_docs=false}) ->
-    add_revs(DbName, Hits);
-maybe_add_revs(_DbName, Hits, _) ->
+maybe_add_docs_in_view_format(DbName, Hits, #h_args{include_docs=false}) ->
+    add_docs_in_view_format(DbName, Hits);
+maybe_add_docs_in_view_format(_DbName, Hits, _) ->
     Hits.
 
 
-add_revs(DbName, Hits) ->
+add_docs_in_view_format(DbName, Hits) ->
     DocIds = [Id || #h_hit{id=Id} <- Hits],
     {ok, Docs} = hastings_util:get_json_docs(DbName, DocIds),
     lists:map(fun(H) ->
         {_, {Doc}} = lists:keyfind(H#h_hit.id, 1, Docs),
-        {_, Rev} = lists:keyfind(<<"_rev">>, 1, Doc),
-        H#h_hit{doc = Rev}
+        H#h_hit{doc = Doc}
     end, Hits).
