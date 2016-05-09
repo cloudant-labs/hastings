a90e3ed18cbac9b4fb55d65da13e0a7159378814
jiangphcn <jiangph@cn.ibm.com>
2016-05-09 17:52:17 +0800

    Add integration test to trigger Geo index builder
    Bugzid: 60763

---
+++ test/hastings_ken_tests.erl
@@ -0,0 +1,62 @@
+%% Copyright 2016 Cloudant
+
+-module(hastings_ken_tests).
+
+
+-include_lib("couch/include/couch_eunit.hrl").
+-include_lib("couch/include/couch_db.hrl").
+
+
+-define(TIMEOUT, 10000).
+
+
+setup() ->
+    DbName = ?tempdb(),
+    ok = fabric:create_db(DbName, [?ADMIN_CTX]),
+    DbName.
+
+
+teardown(DbName) ->
+    ok = fabric:delete_db(DbName, [?ADMIN_CTX]).
+
+
+ken_test_() ->
+    {
+        "Ken Tests",
+        {
+            setup,
+            fun() -> test_util:start_couch([ken, fabric, mem3, hastings]) end,
+            fun test_util:stop/1,
+            {
+                foreach,
+                fun setup/0, fun teardown/1,
+                [
+                    fun ken/1
+                ]
+            }
+        }
+    }.
+
+
+ken(DbName) ->
+    Fn = filename:join([?APPDIR, "test", "testdata", "geo_docs.json"]),
+    {ok, Bin} = file:read_file(Fn),
+    DocsArray = jiffy:decode(Bin),
+    Docs = [couch_doc:from_json_obj(JsonObj) || JsonObj <- DocsArray],
+
+    DesignFn = filename:join([?APPDIR, "test", "testdata",
+        "geo_design.json"]),
+    {ok, Bin2} = file:read_file(DesignFn),
+    JsonDDoc = jiffy:decode(Bin2),
+    DDoc = couch_doc:from_json_obj(JsonDDoc),
+
+    {ok, _} = fabric:update_docs(DbName, Docs, [?ADMIN_CTX]),
+
+    Pid = ets:first(hastings_by_pid),
+    ?_assertEqual(false, is_pid(Pid)),
+
+    {ok, _} = fabric:update_doc(DbName, DDoc, [?ADMIN_CTX]),
+    timer:sleep(500),
+    % After we upload design doc, there should a pid for the index.
+    Pid2 = ets:first(hastings_by_pid),
+    ?_assertEqual(true, is_pid(Pid2)).
