be8040af66bde2fd812fb365abf5cf24e66cfb38
Paul J. Davis <paul.joseph.davis@gmail.com>
2017-04-20 09:10:11 -0500

    Apply ASLv2

--- src/hastings_format_geojson.erl
+++ src/hastings_format_geojson.erl
@@ -1,3 +1,15 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
 -module(hastings_format_geojson).
 -behaviour(hastings_format).
 
@@ -19,11 +31,11 @@ hits_to_json0(Hits, Bookmark) ->
         ExtraProps = case H#h_hit.doc of
             undefined -> [{<<"type">>, <<"Feature">>}, {<<"properties">>, []}];
             {DocList0} ->
-                Props0 = case lists:keyfind(<<"properties">>, 1, DocList0) of 
+                Props0 = case lists:keyfind(<<"properties">>, 1, DocList0) of
                     {_, Props} -> Props;
                 _ -> []
                 end,
-                Rev0 = case lists:keyfind(<<"_rev">>, 1, DocList0) of 
+                Rev0 = case lists:keyfind(<<"_rev">>, 1, DocList0) of
                     {_, Rev} -> Rev;
                     _ -> null
                 end,
--- src/hastings_httpd.erl
+++ src/hastings_httpd.erl
@@ -1,4 +1,14 @@
-%% Copyright 2014 Cloudant
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
 
 -module(hastings_httpd).
 
@@ -121,8 +131,8 @@ get_format(Req, Params) ->
             case chttpd:header_value(Req, <<"Accept">>) of
                 undefined ->
                     hastings_httpd_util:to_format(format, null);
-                Value -> 
-                    try hastings_httpd_util:to_format(format, Value) of 
+                Value ->
+                    try hastings_httpd_util:to_format(format, Value) of
                         Value0 -> Value0
                     catch {_, _} ->
                         hastings_httpd_util:to_format(format, null)
